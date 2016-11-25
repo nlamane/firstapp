9cddb9fc6fd730188ca94c317d305f02c419dd7e
nlamane <noemilamane2@gmail.com>
2016-11-25 16:54:00 +0100

    version three

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :synopsis, :director, :rate)
+      params.require(:post).permit(:title, :synopsis, :director, :rate, :image)
     end
 end
